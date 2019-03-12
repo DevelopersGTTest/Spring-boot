dcf073ae32d3fbb65734f0104f04d1c0184d001d
unknown <pablojacobo1998@gmail.com>
2019-03-11 22:50:00 -0600

    GET Method Controller

---
+++ src/main/java/com/hck/app/controllers/HeroeController.java
@@ -0,0 +1,26 @@
+package com.hck.app.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hck.app.models.entity.Heroes;
+import com.hck.app.models.services.HeroeService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HeroeController {
+	
+	@Autowired
+	private HeroeService heroeService;
+	
+	@GetMapping("/heroes")
+	public List<Heroes> index(){
+		
+		return heroeService.findAll();
+	}
+	
+}
