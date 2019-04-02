081d6595e6a5cb5bcbcff58bc95b12181a2e8964
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-04-01 20:27:42 -0600

    class Dao

--- src/main/java/com/hck/app/models/dao/UsersRepository.java
+++ src/main/java/com/hck/app/models/dao/UsersRepository.java
@@ -1,5 +1,9 @@
 package com.hck.app.models.dao;
 
-public class UsersRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.hck.app.models.entity.Users;
+
+public interface UsersRepository extends CrudRepository<Users , Long >  {
 
 }
